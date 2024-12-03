e5f5c1acc4138031cbdbf9c148bbf80388219bc3
Sumit Gusain <sumitgusain5311@gmail.com>
2024-12-03 07:19:59 +0530

    somechanges

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output:"export",
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
